9dde27f430ecab29784638c9897fdd5ad1a4d591
Daniel Cooke <dcooke@well.ox.ac.uk>
2015-08-26 12:18:54 +0100

    Added helper functions collapse and rebase to Tandem

--- example.cpp
+++ example.cpp
@@ -14,51 +14,6 @@
     the Tandem API.
  */
 
-// Replaces all contiguous sub-sequences of N's with a single N, inplace, and returns a map of
-// each N position in the new sequence, and how many N's have been removed up to the first non-N
-// base past the position
-template <typename SequenceType>
-std::map<std::size_t, std::size_t> collapse_ns(SequenceType& sequence)
-{
-    std::map<std::size_t, std::size_t> result {};
-    
-    auto last = std::end(sequence);
-    std::size_t position {}, num_removed {};
-    
-    for (auto first = std::begin(sequence); first != last;) {
-        auto it1 = std::adjacent_find(first, last, [] (char lhs, char rhs) { return lhs == 'N' && lhs == rhs; });
-        
-        if (it1 == last) break;
-        
-        auto it2 = std::find_if_not(it1, last, [] (char base) { return base == 'N'; });
-        
-        position    += std::distance(first, it1);
-        num_removed += std::distance(it1, it2) - 1;
-        
-        result.emplace(position, num_removed);
-        
-        first = it2;
-    }
-    
-    if (!result.empty()) {
-        sequence.erase(std::unique(std::next(std::begin(sequence), std::cbegin(result)->first), last,
-                                   [] (char lhs, char rhs) { return lhs == 'N' && lhs == rhs; }), last);
-    }
-    
-    return result;
-}
-
-void rebase(std::vector<Tandem::StringRun>& runs, const std::map<std::size_t, std::size_t>& shift_map)
-{
-    if (shift_map.empty()) return;
-    
-    auto shift_map_it = std::cbegin(shift_map);
-    for (auto& run : runs) {
-        while (std::next(shift_map_it)->first <= run.pos) ++shift_map_it;
-        run.pos += static_cast<decltype(run.pos)>(shift_map_it->second);
-    }
-}
-
 template <typename SequenceType>
 bool is_dna(const SequenceType& sequence)
 {
@@ -73,6 +28,8 @@ bool is_rna(const SequenceType& sequence)
 
 void find_big_repeats(std::string& sequence)
 {
+    if (sequence.empty()) return;
+    
     std::cout << "Looking for repeats in " << sequence.size() << "bp" << std::endl;
     
     if (sequence.back() != 'N') {
@@ -80,11 +37,9 @@ void find_big_repeats(std::string& sequence)
         sequence.push_back('N'); // last character must not be in alphabet
     }
     
-    auto n_shift_map = collapse_ns(sequence); // makes search a lot faster
+    auto n_shift_map = Tandem::collapse(sequence, 'N'); // makes search a lot faster
     
     auto repeats = Tandem::find_maximal_repetitions(sequence);
-    
-    rebase(repeats, n_shift_map);
     
     std::cout << "Found " << repeats.size() << " repeats" << std::endl;
     
